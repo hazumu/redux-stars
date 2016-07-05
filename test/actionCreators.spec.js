8368db005e16ce8061e2b4799f9f927bf5f0d202
hazumu <hazumu@r.recruit.co.jp>
2016-07-05 19:16:12 +0900

    star making tests

---
+++ test/actionCreators.spec.js
@@ -0,0 +1,33 @@
+import expect from 'expect';
+import { MOUSEMOVE, UPDATE, INITIALIZE_STAR, DID_STARS_UNMOUNTED } from '../src/actions/actionTypes';
+import { mousemove, update, initializeStar, didStarsUnmounted } from '../src/actions/actionCreators';
+
+describe('actions', () => {
+
+  it('should create mousemove action', () => {
+    const e = {};
+    expect(mousemove(e)).toEqual({
+      type: MOUSEMOVE,
+      e,
+    });
+  });
+
+  it('should create update action', () => {
+    expect(update()).toEqual({
+      type: UPDATE,
+    });
+  });
+
+  it('should create initializeStar action', () => {
+    expect(initializeStar()).toEqual({
+      type: INITIALIZE_STAR,
+    });
+  });
+
+  it('should create didStarsUnmounted action', () => {
+    expect(didStarsUnmounted()).toEqual({
+      type: DID_STARS_UNMOUNTED,
+    });
+  });
+
+});
---
+++ test/reducers.spec.js
@@ -0,0 +1,10 @@
+import expect from 'expect';
+import { MOUSEMOVE, UPDATE, INITIALIZE_STAR, DID_STARS_UNMOUNTED } from '../src/actions/actionTypes';
+import reducer from '../src/reducers/index';
+
+describe('reducer', () => {
+
+  it('should initialize state to {}', () => {
+  });
+
+});
